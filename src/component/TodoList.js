8f237c63854ef683f481288c41de770f0b87d855
sadafamininia99 <aminicomputer96@gmail.com>
2022-01-08 12:10:54 +0330

    store data till now on console (id +text {form input by user }

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -3,10 +3,19 @@ import TodoForm from "./TodoForm";
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
+
+const addTodo =todo =>{
+    if (!todo.text || /^\s*$/.test(todo.text)){
+        return 
+    }
+    const newTodos  =[todo ,...todos]
+    setTodos(newTodos)
+    console.log(...todos);
+}
   return (
     <div>
       <h1>What is the paln for Today ?</h1>
-      <TodoForm />
+      <TodoForm onSubmit={addTodo} />
     </div>
   );
 }
