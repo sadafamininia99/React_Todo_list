175e68442d198df4f9df9c62b679d790b4e44820
sadafamininia99 <aminicomputer96@gmail.com>
2022-01-08 12:00:14 +0330

    form

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,1 +1,14 @@
- 
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  return (
+    <div>
+      <h1>What is the paln for Today ?</h1>
+      <TodoForm />
+    </div>
+  );
+}
+
+export default TodoList;
